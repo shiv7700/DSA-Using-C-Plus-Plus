6c4c81d623cb2398c9b5452a33d68d60dffafe87
ShivanG <89482670+shiv7700@users.noreply.github.com>
2022-10-18 17:42:25 +0530

    Add files via upload

---
+++ basic/first.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    cout<<"namaste duniya";
+    return 0;
+
+    string myvariable="this is a string";
+    cout<<myvariable;
+
+    short int myvariable=40000;
+    cout<<myvariable;
+
+    unsigned int a=-100;
+    cout<<a;
+
+    int a=10;
+
+    if(a=10){
+        cout<<"true";
+    } else {
+        cout<<"false";
+    }
+
+
+    int x=2;
+    int y=5;
+    int result=y%x;
+
+    cout<<result;
+
+
+    int a=10;
+    int b=20;
+
+    if(a=10 || a>5){
+        cout<<"true";
+    } else {
+        cout<<"false";
+    }
+
+    cout<<"welcome to dsa";
+    cout<<"\n";
+    cout<<"okay";
+
+
+
+    return 0;
+
+
+
+
+}
